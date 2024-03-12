96a2e086434cbb266d6f6f03d104fcb2383dd175
Santiago Cecena <santycc00@hotmail.com>
2024-03-12 01:00:12 -0700

    Build dir fixed

--- build/utils.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isError = void 0;
-const isError = (error) => error instanceof Error;
-exports.isError = isError;
