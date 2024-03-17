1119a87df1aedd9d16fc7530eb66c28b87051603
Santiago Cecena <santycc00@hotmail.com>
2024-03-16 22:48:09 -0700

    Manejo de error en en login corregido

--- src/middleware/login-user.ts
+++ src/middleware/login-user.ts
@@ -23,14 +23,18 @@ export const isAuth = ( req: any, res: any, next: any ) => {
 
     } catch (err: unknown) {
         if (!(err instanceof Error)) return;
-        const error = new Error("Something went wrong")
-        res.status(500);
-        next(error);
+        const error = new Error("The token is invalid")
+        res.status(401);
+        res.send(error.message);
+        return;
+        //* La manera de manejar errores con next() es una manera vieja, podemos enviar el error en la response y express lo maneja automaticamente.
+        // next(error);
     }
     if(!decodedToken) {
         const error = new Error("Not authenticated");
         res.status(401);
-        next(error);
+        res.send(error.message)
+        // next(error);
     }
     // req.userId = decodedToken.user;
     next();
